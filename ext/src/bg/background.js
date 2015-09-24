8adf048983f13db3f46f70c5c12ed4c251bf365e
jrrera <jrrera@gmail.com>
2015-09-24 14:02:44 -0700

    feat(InspireTab): adds inspiring images and daily counts support
    We now have a rotating selection of inspiring images to persuade you
    intrinsically to not go on social media. Daily counts also make it more
    realistic (showing all time count loses its meaning rather quickly).

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -9,8 +9,23 @@ var urlPatterns = {
   reddit: /^https?:\/\/(www)?\.?reddit.com/,
 };
 
+var disableDurationInMinutes = 5;
+
+/**
+ * Helper function that generates a date string YYYYMMDD as storage key.
+ * @return {string} Date string in form of YYYYMMDD.
+ */
+function getCountsKey() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; // 0 based months.
+  var yyyy = today.getFullYear();
+  return yyyy + mm + dd;
+}
+
 chrome.tabs.onUpdated.addListener(function(tabId, status, changeInfo) {
-  var counts, site;
+  var counts, site,
+      countsKey = getCountsKey();
 
   // console.log('tab changed!', status, changeInfo);
 
@@ -24,10 +39,11 @@ chrome.tabs.onUpdated.addListener(function(tabId, status, changeInfo) {
 
         // Get access to total number of accesses and send it as query string
         // along with redirect URL and sitename.
-        counts = store.get('counts') || {};
+        counts = store.get(countsKey) || {};
         chrome.tabs.update(tabId, {
           url: 'src/interrupt/interrupt.html?redirect=' + encodeURIComponent(changeInfo.url) +
-               '&site=' + site + '&count=' + (counts[site] || 0)
+               '&site=' + site + '&count=' + (counts[site] || 0) +
+               '&minutes=' + disableDurationInMinutes
         });
       }
     }
@@ -35,10 +51,11 @@ chrome.tabs.onUpdated.addListener(function(tabId, status, changeInfo) {
 });
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+  countsKey = getCountsKey();
   if (msg.allowEntry) {
     disabled = true;
 
-    var counts = store.get('counts') || {};
+    var counts = store.get(countsKey) || {};
 
     if (counts[currentSite]) {
       counts[currentSite]++;
@@ -46,11 +63,11 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
       counts[currentSite] = 1; // init
     }
 
-    store.set('counts', counts);
+    store.set(countsKey, counts);
 
     setTimeout(function() {
       disabled = false;
-    }, msg.allowFor || (1 * 60 * 1000));
+    }, msg.allowFor || (disableDurationInMinutes * 60 * 1000));
 
     sendResponse({ 'success': true });
   }
--- ext/src/interrupt/interrupt.js
+++ ext/src/interrupt/interrupt.js
@@ -1,19 +1,31 @@
 angular.module('inspiretab.interrupt', [])
-	.config(function($locationProvider) {
+	.config(function($locationProvider, $compileProvider) {
+		// Support for query string given by bg script.
 		$locationProvider.html5Mode(true).hashPrefix('!');
+
+		// Support chrome extensions in ng-src and ng-href for angular.
+		$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
+		$compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|local|data|chrome-extension):/);
 	})
 	.directive('itInterruptDialogue', function($location, $window) {
-		console.log('running!');
 		return {
 			restrict: 'EA',
 			transclude: true,
 			link: function(scope, elem, attrs, ctrl, transclude) {
 				var queryData = $location.search();
 
+				var imgList = [
+					'http://www.livingforimprovement.com/wp-content/uploads/2012/06/gsummit-action-shot.jpg',
+					'chrome-extension://igmjihpajekbbigheciochonooocbljb/img/jon-featured.png',
+					'chrome-extension://igmjihpajekbbigheciochonooocbljb/img/jon-gabe-chat.png'
+				];
+
+				var randomImage =  imgList[ Math.floor(Math.random() * imgList.length) ];
+
 				scope.numberOfTimes = queryData.count;
 				scope.currentSite = queryData.site;
-
-				scope.inspireImage = 'http://www.livingforimprovement.com/wp-content/uploads/2012/06/gsummit-action-shot.jpg';
+				scope.inspireImage = randomImage;
+				scope.minutesAllowed = queryData.minutes;
 
 				transclude(scope, function(clone, scope) {
 					elem.append(clone);
